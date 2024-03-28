9e25a9a2884e715aaf2b766d02469de15273ed7b
tetsu555 <tetete1118@gmail.com>
2024-03-28 13:25:15 +0900

    fix middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+  //       destination: '<YOUR REDIRECT RULE>',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
+  // async rewrites() {
+  //   return {
+  //     beforeFiles: [
+  //       {
+  //         source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+  //         destination: '<YOUR REWRITE RULE>',
+  //       }
+  //     ]
+  //   }
+  // },
+};
 
 export default nextConfig;

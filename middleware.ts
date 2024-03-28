316db1971c23b99b690083e6bad0c08048e8d0ea
tetsu555 <tetete1118@gmail.com>
2024-03-28 13:26:33 +0900

    fix

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   // console.log('middleware.ts!!!!', request)
-  return NextResponse.redirect(new URL('/home', request.url))
+  // return NextResponse.redirect(new URL('/home', request.url))
 }
 
 // See "Matching Paths" below to learn more
